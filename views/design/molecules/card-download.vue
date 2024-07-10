9a7844584d1c07a0406ea24415ac84a2bce1ada7
monkeyacosta <aacosta@monkeyplusbc.com>
2024-07-10 12:12:16 -0500

    card download modificado

--- views/design/molecules/card-download.vue
+++ views/design/molecules/card-download.vue
@@ -10,7 +10,7 @@ defineProps({
   <div class="card-download">
     <mk-picture e-width="85" e-height="85" :src="data?.icon?.src" :alt="data?.icon?.alt" :title="data?.icon?.title" />
     <div class="title" v-html="data?.title" />
-    <AtomsButton :data="data?.link">
+    <AtomsButton :data="data?.btn.link">
       <MoleculesBtn :data="data?.btn" />
     </AtomsButton>
   </div>
